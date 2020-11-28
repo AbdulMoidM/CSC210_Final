72aa3efc89ca9bdb832b784ff8f9a0ed8134b30d
xchen92 <42846715+xchen92@users.noreply.github.com>
2020-11-28 13:20:30 -0500

    line7 add userSchema

--- app.js
+++ app.js
@@ -5,6 +5,26 @@ const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
+mongoose.connect(process.env.MONGO_DATA);
+mongoose.set("useCreateIndex", true);
+const secretSchema = new mongoose.Schema({
+  secretString: String
+});
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: false
+  },
+  password: String,
+  googleId: String,
+  facebookId: String,
+  secret: [{
+    type: secretSchema
+  }]
+});
+const Secret = mongoose.model("Secret", secretSchema);
+const User = mongoose.model("User", userSchema);
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({
