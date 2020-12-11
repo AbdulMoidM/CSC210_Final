9e334c2815dcc1a7caa69bed0c2e2ce1f7b67ed8
AbdulMoidM <53446831+AbdulMoidM@users.noreply.github.com>
2020-12-12 01:12:25 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 //jshint esversion:6
 require('dotenv').config()
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
+
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
@@ -22,6 +29,20 @@ const userSchema = new mongoose.Schema({
     type: secretSchema
   }]
 });
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+passport.use(User.createStrategy());
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
 const Secret = mongoose.model("Secret", secretSchema);
 const User = mongoose.model("User", userSchema);
 
@@ -31,6 +52,14 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(express.static("public"));
+app.use(session({
+  secret: "this is my secret",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.listen(process.env.PORT || 3000, function() {
   console.log("Server is up and running");
